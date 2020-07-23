c08c23d03ed48887f558309c2dec4d090a1d11cf
unknown <ganiuolalekan2000@yahoo.com>
2020-07-23 15:45:51 +0100

    make it better

--- index/urls.py
+++ index/urls.py
@@ -3,7 +3,7 @@ from . import views
 # from django.urls import path, include
 
 urlpatterns = [
-	url(r'^/$', views.index, name='index'), # login url
+	url(r'^$', views.index, name='index'), # login url
 	url(r'^account/dajsyewowq29aeqcv/$', views.account, name='account'),
 	url(r'^signoff/$', views.logout, name='signoff'),
 	# url('account/(?P<job>[A-Za-z]+)/$', views.dosomething, name='dosomething'),
