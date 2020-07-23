479560b508ec094129d70fad119b10b360068295
unknown <ganiuolalekan2000@yahoo.com>
2020-07-23 06:31:20 +0100

    Initial commit

--- index/urls.py
+++ index/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
+# from django.conf.urls import url
 from . import views
+from django.urls import path, include
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'), # login url
-	url(r'^account/dajsyewowq29aeqcv/$', views.account, name='account'),
-	url(r'^signoff/$', views.logout, name='signoff'),
-	url(r'^account/(?P<job>[A-Za-z]+)/$', views.dosomething, name='dosomething'),
+	path('', views.index, name='index'), # login url
+	path('account/dajsyewowq29aeqcv/', views.account, name='account'),
+	path('signoff/', views.logout, name='signoff'),
+	# url('account/(?P<job>[A-Za-z]+)/$', views.dosomething, name='dosomething'),
 ]
